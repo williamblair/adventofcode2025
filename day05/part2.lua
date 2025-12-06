69117e3e46e55e553d50dc21d07c2e6759856957
williamblair <bblair12876@aol.com>
2025-12-06 01:09:42 -0500

    finish day5 part 2 (for now...)

--- day05/part2.lua
+++ day05/part2.lua
@@ -1,8 +1,36 @@
-function getTotalFreshCount(ranges) do
-    fresh = {}
-    for idx, range in ipairs(ranges) do
-        for i = 
+function overlaps(minA, maxA, minB, maxB)
+    return minA <= maxB and minB <= maxA
+end
+
+function combineOverlappingRanges(ranges)
+    local minA, maxA = next(ranges)
+    while minA do
+        local minB, maxB = next(ranges, minA)
+        local tmpMinB, tmpMaxB = minB, maxB
+        while (tmpMinB ~= nil and tmpMaxB ~= nil) do
+            if (overlaps(minA, maxA, tmpMinB, tmpMaxB)) then
+                ranges[minA] = nil
+                ranges[tmpMinB] = nil
+                ranges[math.min(minA,tmpMinB)] = math.max(maxA,tmpMaxB)
+                return true
+            end
+            tmpMinB, tmpMaxB = next(ranges, tmpMinB)
+        end
+        minA, maxA = minB, maxB
     end
+    return false
+end
+
+function getTotalFreshCount(ranges)
+    local didCombine = true
+    while (didCombine) do
+        didCombine = combineOverlappingRanges(ranges)
+    end
+    local sum = 0
+    for min, max in pairs(ranges) do
+        sum = sum + (max - min) + 1
+    end
+    return sum
 end
 
 testRangesStrs = {
@@ -16,23 +44,29 @@ for idx, rangeStr in ipairs(testRangesStrs) do
     local beginStr, endStr = rangeStr:match("(.+)-(.+)")
     local beginVal = tonumber(beginStr)
     local endVal = tonumber(endStr)
-    testRanges[idx] = {}
-    testRanges[idx][0] = beginVal
-    testRanges[idx][1] = endVal
+    testRanges[beginVal] = endVal
+end
+local testCount = getTotalFreshCount(testRanges)
+if (testCount ~= 14) then
+    print("Total fresh count failed:", testCount)
+    return
+else
+    print("Total fresh count passed")
 end
 
---inRangeFile = io.open(arg[1], "r")
---inRanges = {}
---idx = 0
---for line in inRangeFile:lines() do
---    local beginStr, endStr = line:match("(.+)-(.+)")
---    local beginVal = tonumber(beginStr)
---    local endVal = tonumber(endStr)
---    inRanges[idx] = {}
---    inRanges[idx][0] = beginVal
---    inRanges[idx][1] = endVal
---    idx = idx + 1
---end
+if (arg[1] == nil) then
+    print("Usage: part2.lua <rangefile>")
+    return
+end
 
-print("Fresh count:",getFreshCount(inRanges, inIngredients))
+inRangeFile = io.open(arg[1], "r")
+inRanges = {}
+for line in inRangeFile:lines() do
+    local beginStr, endStr = line:match("(.+)-(.+)")
+    local beginVal = tonumber(beginStr)
+    local endVal = tonumber(endStr)
+    inRanges[beginVal] = endVal
+end
+
+print("Fresh count:", getTotalFreshCount(inRanges))
 
